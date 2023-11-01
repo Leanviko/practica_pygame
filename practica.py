dd6f31ef49537355d6cb95aeaaee571ecd6bc22f
unknown <leangant@gmail.com>
2023-11-01 16:14:55 -0300

    fondo y sprite de nave

--- practica.py
+++ practica.py
@@ -12,6 +12,8 @@ HEIGTH = 500
 pantalla = pygame.display.set_mode((WEIGHT, HEIGTH))
 #* reloj 
 reloj = pygame.time.Clock() 
+background = pygame.image.load("Images/Nebula.png").convert()
+nave = pygame.image.load("Images/nave.png").convert_alpha()
 
 #*propiedades del cuadrado
 cord_x = 300
@@ -24,32 +26,26 @@ cord_y = 200
 pygame.mouse.set_visible(False) #Esconde el puntero en la pantalla
 #bucle principal
 
+
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
         
     ###-----INICIO LOGICA----###
-    #* limites al movimiento
-    # if(cord_x > 720 or cord_x < 0):
-    #     velocidad_x *= -1
-    # if(cord_y > 420 or cord_y < 0):
-    #     velocidad_y *= -1
-
-    # cord_x += velocidad_x
-    # cord_y += velocidad_y
+    
     mouse_posicion = pygame.mouse.get_pos()
-    print(mouse_posicion)
     cord_x = mouse_posicion[0]
     cord_y = mouse_posicion[1]
-    ###-----INICIO LOGICA----###
-    
-    ###-----FIN LOGICA----###
+    ###-----fin LOGICA----###
 
-    pantalla.fill((150,255,100)) #* pinta la pantalla del color
+    pantalla.blit(background, (0,0))
+    pantalla.blit(nave,(cord_x,cord_y)) 
+    
     ###-----INICIO ZONA DE DIBUJO----###
     
-    pygame.draw.rect(pantalla, (200,10,50), (cord_x,cord_y, 80, 80))
+    
 
     
 
@@ -58,3 +54,16 @@ while True:
     reloj.tick(60) #*fps
 
 
+#* limites al movimiento
+    # if(cord_x > 720 or cord_x < 0):
+    #     velocidad_x *= -1
+    # if(cord_y > 420 or cord_y < 0):
+    #     velocidad_y *= -1
+
+    # cord_x += velocidad_x
+    # cord_y += velocidad_y
+
+#* pinta la pantalla del color
+    #pantalla.fill((150,255,100)) 
+
+#pygame.draw.rect(pantalla, (200,10,50), (cord_x,cord_y, 80, 80))
