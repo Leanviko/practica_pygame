7d6cf46cdacc152a06336ffa905089c549c82676
unknown <leangant@gmail.com>
2023-11-02 12:55:06 -0300

    sprite enemigo y movimiento basico

--- practica.py
+++ practica.py
@@ -1,5 +1,50 @@
 import pygame, sys, random
 
+class Nave(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Images/nave.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDHT/2
+        self.rect.bottom = HEIGTH - 30
+        self.mover_derecha = False
+        self.mover_izquierda = False
+        self.mover_arriba = False
+        self.mover_abajo = False
+        
+    def update(self):
+        
+        if self.mover_derecha and self.rect.right < WIDHT:
+            self.rect.x += 3
+        if self.mover_izquierda and self.rect.left > 0:
+            self.rect.x -= 3
+        if self.mover_arriba and self.rect.top > 0:
+            self.rect.y -= 3
+        if self.mover_abajo and self.rect.bottom < HEIGTH:
+            self.rect.y += 3
+
+class Enemigo(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Images/enemigo.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = random.randrange(100,500)
+        self.rect.bottom = -10
+        
+        self.velocidad_x = 4
+
+    def update(self):
+        self.rect.y += 2
+        self.rect.x += self.velocidad_x
+        if self.rect.right > 550 or self.rect.left < 50:
+            self.velocidad_x *= -1
+        elif self.rect.top > 800:
+            self.rect.x = random.randrange(100,500)
+            self.rect.bottom = -10
+
+    
+
 class Meteoro(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -9,25 +54,33 @@ class Meteoro(pygame.sprite.Sprite):
     def update(self):
         
         self.rect.y += 2
+        meteoro_colision_lista = pygame.sprite.spritecollide(nave, meteoro_lista, True)
+
         if self.rect.top > 800:
             self.rect.bottom = -10
             self.rect.x = random.randrange(WIDHT)
 
-        
 
 
-class Player(pygame.sprite.Sprite):
+class Laser(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Images/nave.png").convert_alpha()
+        self.image = pygame.image.load("Images/laser.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = WIDHT/2
-        self.rect.bottom = HEIGTH - 30
-    
+
+        
     def update(self):
-        # mouse_posicion = pygame.mouse.get_pos()
-        self.rect.x += velocidad_x
-        self.rect.y += velocidad_y
+        self.rect.y -= 11
+        for laser in laser_lista:
+            meteoro_colision_lista = pygame.sprite.spritecollide(laser, meteoro_lista, True)
+            for meteoro in meteoro_colision_lista:
+                todos_sprites_lista.remove(laser)
+                laser_lista.remove(laser)
+        if laser.rect.bottom < -10:
+            todos_sprites_lista.remove(laser)
+            laser_lista.remove(laser)
+        
+            
 
 
 pygame.init()
@@ -35,9 +88,7 @@ pygame.init()
 WIDHT = 600
 HEIGTH = 800
 
-#* creamos la ventana
-pantalla = pygame.display.set_mode((WIDHT, HEIGTH))
-#* reloj 
+pantalla = pygame.display.set_mode((WIDHT, HEIGTH)) 
 reloj = pygame.time.Clock() 
 background = pygame.image.load("Images/Nebula.png").convert()
 
@@ -46,22 +97,27 @@ puntaje = 0
 
 #*grupos de sprites
 meteoro_lista = pygame.sprite.Group()
+laser_lista = pygame.sprite.Group()
+enemigos_lista = pygame.sprite.Group()
 todos_sprites_lista = pygame.sprite.Group()
 
-for i in range(11):
-    meteoro = Meteoro()
-    meteoro.rect.x = random.randrange(WIDHT)
-    meteoro.rect.y = random.randrange(HEIGTH)
+for i in range(10):
+        meteoro = Meteoro()
+        meteoro.rect.x = random.randrange(WIDHT)
+        meteoro.rect.y = random.randrange(HEIGTH)
+        meteoro_lista.add(meteoro)
+        todos_sprites_lista.add(meteoro)
 
-    meteoro_lista.add(meteoro)
-    todos_sprites_lista.add(meteoro)
+enemigo = Enemigo()
+enemigos_lista.add(enemigo)
+todos_sprites_lista.add(enemigo)
+
 
 #* nave como variable
-nave = Player()
+nave = Nave()
 todos_sprites_lista.add(nave)
+laser = Laser()
 
-velocidad_x = 0
-velocidad_y = 0
 
 #mouse
 pygame.mouse.set_visible(False) #Esconde el puntero en la pantalla
@@ -71,46 +127,45 @@ while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
-        print(evento)
-        keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_LEFT]:
-            velocidad_x = -2.5
-        if keys[pygame.K_RIGHT]:
-            velocidad_x = 2.5
-        if keys[pygame.K_UP]:
-            velocidad_y -= 2.5
-        if keys[pygame.K_DOWN]:
-            velocidad_y += 2.5
-        if keys[pygame.K_LCTRL]:
-            
-                velocidad_x = 0
-                velocidad_y = 0
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT: 
+                nave.mover_izquierda = True
+            if evento.key == pygame.K_RIGHT:
+                nave.mover_derecha = True
+            if evento.key == pygame.K_UP:
+                nave.mover_arriba = True
+            if evento.key == pygame.K_DOWN:
+                nave.mover_abajo = True
+            if evento.key == pygame.K_SPACE:
+                laser = Laser()
+                laser.rect.center = nave.rect.center
+                laser.rect.bottom = nave.rect.top
+                laser_lista.add(laser)
+                todos_sprites_lista.add(laser)
 
-        # if evento.type == pygame.KEYUP:
-        #     if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
-        #         velocidad_x = 0
-        #     if evento.key == pygame.K_UP or evento.key == pygame.K_DOWN:
-        #         velocidad_y = 0
+        elif evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT: 
+                nave.mover_izquierda = False
+            if evento.key == pygame.K_RIGHT:
+                nave.mover_derecha = False
+            if evento.key == pygame.K_UP:
+                nave.mover_arriba = False
+            if evento.key == pygame.K_DOWN:
+                nave.mover_abajo = False
+            
+        
         
     ###-----INICIO LOGICA----###
     
+    
     todos_sprites_lista.update()
-
-                #*colision con los meteoros
-    meteoro_colision_lista = pygame.sprite.spritecollide(nave, meteoro_lista, True)
-
-    for meteoro in meteoro_colision_lista:
-        puntaje += 1
-        print(puntaje)
-
     ###-----fin LOGICA----###
     pantalla.blit(background, (0,0))
     ###-----INICIO ZONA DE DIBUJO----###
 
     todos_sprites_lista.draw(pantalla)
     
-
     
 
     ###-----FIN ZONA DE DIBUJO----###
@@ -118,19 +173,6 @@ while True:
     reloj.tick(60) #*fps
 
 
-#* limites al movimiento
-    # if(cord_x > 720 or cord_x < 0):
-    #     velocidad_x *= -1
-    # if(cord_y > 420 or cord_y < 0):
-    #     velocidad_y *= -1
 
-    # 
 
-#* pinta la pantalla del color
-    #pantalla.fill((150,255,100)) 
 
-#pygame.draw.rect(pantalla, (200,10,50), (cord_x,cord_y, 80, 80))
-
-#*velocidad del cuadrado
-# velocidad_x = 3
-# velocidad_y = 3
